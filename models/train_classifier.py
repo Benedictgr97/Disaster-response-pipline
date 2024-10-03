90f54d72f34e886071d6a6e9102b68ca4bd7745f
Benedict grant <64258751+Benedictgr97@users.noreply.github.com>
2024-10-03 11:47:48 +0100

    a

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,9 +1,56 @@
-import sys
+"""
+Disaster reponse clasification training
+
+Arguments: 
+    1. Pickle file output location 
+
+Outputs: 
+    1. Pickle file of model
+
+"""
+
+
+# import libraries
+import pandas as pd
+import numpy as np 
+
+import re
+from sqlalchemy import create_engine
+
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.base import BaseEstimator,TransformerMixin
+
+import xgboost as xgb
+
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer, PorterStemmer
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('punkt_tab')
+nltk.download('stopwords')
+nltk.download('averaged_perceptron_tagger_eng')
 
 
 def load_data(database_filepath):
-    pass
+    filepath = "../data/Response_db.db"
+    engine = create_engine('sqlite:///' + filepath)
+    df = pd.read_sql_table('DISASTER_RESPONSE_TABLE',engine)
 
+    # Child alone has 0 for all values so drop 
+    # Realted has a max of 2, most likley a fluke for a binary input 
+    # Only Nulls in originl and this will be dropped as its not usefull in this context 
+    df.drop('child_alone',axis = 1,inplace=True)
+    df['related'] = df['related'].apply(lambda x: 1 if x > 0 else 0)
+    
 
 def tokenize(text):
     pass
